a2659a5395bebe5019021f51ca91bca01a98ada1
Kevin Kafuku <kevkafuku@gmail.com>
2018-06-06 01:51:19 -0400

    jquery started, more logic needed into it it, will get to it in the morning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,24 @@
+// we are creating a trivia game // there are 8 questions // questions have a timer //
+var timeRemaining = 60;
+var right = 0;
+var wrong = 0;
+var unanswered = 0;
+var intervalId;
 
-$("#startbutton").click(function() {
-    $(".game-border").toggle();
-});
+// on button click remove invisible class on game-border // we would also apply invisible class to myborder-2 //
+// on button click create timer //
+// after timer is finished log results of questions wrong and questions right //
+
+function pageLoad() {
+    $(".game-border").hide();
+    $(".my-border3").hide();
+    $("#startbutton").on("click", startGame);
+   
+}
+
+function startGame() {
+    $(".game-border").show();
+    $(".my-border2").show();
+    $("#shotclock").text("SHOT CLOCK : 00:" +  timeRemaining);
+    run();
+}
